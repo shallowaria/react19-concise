5fc4db1e3159903dd4a4893b984083abbc4ed639
shallowaria <shallowaria@gmail.com>
2026-06-21 20:45:03 +0800

    feat: 完成 chanllenge1 和 chanllenge2

--- 2026/part1/challenge-01/start/main.js
+++ 2026/part1/challenge-01/start/main.js
@@ -10,17 +10,38 @@
 */
 
 function MyApp() {
+  const [isPurple, setIsPurple] = React.useState(false);
+  const [textColor, setTextColor] = React.useState('text-white');
+  const [circleSize, setCircleSize] = React.useState(150);
+  const [circleRotate, setCircleRotate] = React.useState(0);
+
+  const circleClassNames = `circle ${isPurple ? 'purple' : ''} ${textColor}`;
+
+  const circleStyles = {
+    height: `${circleSize}px`,
+    width: `${circleSize}px`,
+    lineHeight: `${circleSize}px`,
+    transform: `rotate(${circleRotate}deg)`,
+  };
+
   return (
     <main>
       <label>
         Purple
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={isPurple}
+          onChange={(e) => setIsPurple(e.target.checked)}
+        />
       </label>
 
       <label>
         text color
-        <select>
-          <option value="" selected>
+        <select
+          value={textColor}
+          onChange={(e) => setTextColor(e.target.value)}
+        >
+          <option value="text-white" selected>
             White
           </option>
           <option value="text-black">Black</option>
@@ -30,19 +51,29 @@ function MyApp() {
 
       <label>
         Circle Size
-        <input type="number" value="150" />
+        <input
+          type="number"
+          value={circleSize}
+          onChange={(e) => setCircleSize(Number(e.target.value))}
+        />
       </label>
 
       <label>
         Circle Rotate
-        <input type="number" value="0" />
+        <input
+          type="number"
+          value={circleRotate}
+          onChange={(e) => setCircleRotate(Number(e.target.value))}
+        />
       </label>
-      <div className="circle">Hi!</div>
+      <div className={circleClassNames} style={circleStyles}>
+        Hi!
+      </div>
     </main>
   );
 }
 
-const appEl = document.querySelector("#app");
+const appEl = document.querySelector('#app');
 const root = ReactDOM.createRoot(appEl);
 
 root.render(<MyApp />);
--- 2026/part1/challenge-02/starter/main.js
+++ 2026/part1/challenge-02/starter/main.js
@@ -4,31 +4,86 @@
 */
 
 function MyApp() {
+  const [todoList, setTodoList] = React.useState([]);
+  const [currentTodo, setCurrentTodo] = React.useState('');
+
+  function handleAddTodo() {
+    if (!currentTodo.trim().length) {
+      alert('Please enter a todo item');
+      return;
+    }
+
+    const newTodoList = [
+      ...todoList,
+      {
+        id: Math.random() + Date.now(),
+        content: currentTodo,
+        isCompleted: false,
+      },
+    ];
+
+    setTodoList(newTodoList);
+    setCurrentTodo('');
+  }
+
+  function handleDeleteTodo(deleteTodoId) {
+    const deletedTodoList = todoList.filter(
+      (todo) => todo.id !== Number(deleteTodoId),
+    );
+    setTodoList(deletedTodoList);
+  }
+
+  function toggleTodo(toggleTodoId) {
+    const toggledTodoList = todoList.map((todo) => {
+      if (todo.id === Number(toggleTodoId)) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+
+    sortTodoList(toggledTodoList);
+
+    setTodoList(toggledTodoList);
+  }
+
+  function sortTodoList(newTodoList) {
+    const sorttedTodoList = newTodoList.sort((o1, o2) => {
+      if (o1.isCompleted !== o2.isCompleted) {
+        return o1.isCompleted ? 1 : -1; // 正数排后面，负数排前面
+      }
+      return o1.id - o2.id; // id小的排前面
+    });
+    setTodoList(sorttedTodoList);
+  }
+
   return (
     <main>
       <h1>React Todo List</h1>
-      <input type="text" placeholder="Add item into as todo" />
-      <button>Add</button>
+      <input
+        type="text"
+        placeholder="Add item into as todo"
+        value={currentTodo}
+        onChange={(e) => setCurrentTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add</button>
       <ul>
-        <li className="deleted">
-          <input type="checkbox" />
-          Learn React <button>delete</button>
-        </li>
-        <li>
-          Learn Angular <button>delete</button>
-        </li>
-        <li>
-          Learn Vue <button>delete</button>
-        </li>
-        <li>
-          Learn Svelte <button>delete</button>
-        </li>
+        {todoList.length > 0 &&
+          todoList.map((todo) => (
+            <li className={todo.isCompleted ? 'deleted' : ''} key={todo.id}>
+              <input type="checkbox" onChange={() => toggleTodo(todo.id)} />
+              {todo.content}
+              <button onClick={() => handleDeleteTodo(todo.id)}>delete</button>
+            </li>
+          ))}
       </ul>
     </main>
   );
 }
 
-const appEl = document.querySelector("#app");
+const appEl = document.querySelector('#app');
 const root = ReactDOM.createRoot(appEl);
 
 root.render(<MyApp />);
