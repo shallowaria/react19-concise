c78a67edab80efc0e1d95e399e489bcc2a33da88
shallowaria <shallowaria@gmail.com>
2026-06-27 22:40:34 +0800

    feat: 简单的 useEffect 介绍和接口调用

--- 2026/part2/section7/starter/src/App.jsx
+++ 2026/part2/section7/starter/src/App.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+  const adviceURL = 'https://api.adviceslip.com/advice';
+
+  const [advice, setAdvice] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function getAdvice() {
+    setIsLoading(true);
+
+    const response = await fetch(adviceURL);
+    const data = await response.json();
+
+    const adviceText = data.slip.advice;
+
+    setIsLoading(false);
+    setAdvice(adviceText);
+  }
+
+  useEffect(() => {
+    getAdvice(); // 以同步代码方式调用，不允许任何 Promise 语法，避免在 useEffect 中使用 async/await
+  }, []);
+
   return (
     <main>
       <h1>Advice App</h1>
-      <p></p>
-      <button>Get Advice</button>
+      <p>{isLoading ? 'Loading...' : advice}</p>
+      <button onClick={getAdvice} disabled={isLoading}>
+        Get Advice
+      </button>
     </main>
   );
 }
