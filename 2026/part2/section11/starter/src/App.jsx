c5793d4df6c744c64165996de1b34e3b9a10d57a
shallowaria <shallowaria@gmail.com>
2026-06-29 00:48:41 +0800

    refactor: 将 useTime 抽象为自定义 hook

--- 2026/part2/section11/starter/src/App.jsx
+++ 2026/part2/section11/starter/src/App.jsx
@@ -1,30 +1,37 @@
-import { useState, useEffect } from 'react';
 import useSWR from 'swr';
+import useTime from './useTime';
 
 function App() {
   const adviceURL = 'https://api.adviceslip.com/advice';
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, isLoading, mutate: getAdvice } = useSWR(adviceURL, fetcher);
+  const {
+    data,
+    isLoading,
+    isValidating,
+    mutate: getAdvice,
+  } = useSWR(adviceURL, fetcher);
 
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+  // const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date().toLocaleString());
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentTime(new Date().toLocaleString());
+  //   }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
+  const currentTime = useTime();
 
   return (
     <main>
       <h1>Advice App</h1>
       <span>{currentTime}</span>
       <p>{isLoading ? 'Loading...' : data.slip?.advice}</p>
-      <button disabled={isLoading} onClick={getAdvice}>
+      <button disabled={isLoading || isValidating} onClick={() => getAdvice()}>
         Get Advice
       </button>
     </main>
